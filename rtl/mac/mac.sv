fa8d1e7fde301ab33b969ba11bee23c6ef10fe57
Raveem KG <gkraveem@gmail.com>
2026-02-05 16:38:14 +0530

    D02: Simple 8-bit MAC unit

--- rtl/mac/mac.sv
+++ rtl/mac/mac.sv
@@ -1,9 +1,23 @@
-module mac (
+module mac #(
+    parameter A_WIDTH = 8,
+    parameter B_WIDTH = 8,
+    parameter ACC_WIDTH = 32
+)(
     input  logic clk,
-    input  logic rst,
-    input  logic signed [15:0] a,
-    input  logic signed [15:0] b,
+    input  logic rst_n,        //active-low reset
     input  logic en,
-    output logic signed [31:0] y
+
+    input  logic signed [A_WIDTH-1:0] a,
+    input  logic signed [B_WIDTH-1:0] b,
+    output logic signed [ACC_WIDTH-1:0] acc 
 );
+
+    always_ff @(posedge clk) begin
+        if (!rst_n) begin
+            acc <= 0;
+        end else if(en)begin
+            acc <= acc + (a * b);
+        end
+    end
+
 endmodule
--- tb/basic_tb.sv
+++ tb/basic_tb.sv
@@ -1,12 +1,59 @@
 module basic_tb;
-  logic clk = 0;
-  logic rst = 1;
+
+  logic clk;
+  logic rst_n;
+  logic en;
+  logic signed [7:0] a, b;
+  logic signed [31:0] acc;
+
+  // DUT
+  mac dut(
+    .clk(clk),
+    .rst_n(rst_n),
+    .en(en),
+    .a(a),
+    .b(b),
+    .acc(acc)
+  );
+
+  // Clock
   always #5 clk = ~clk;
 
-  top dut(.clk(clk), .rst(rst));
-
   initial begin
-    #20 rst = 0;
-    #100 $finish;
+    clk = 0;
+    rst_n = 0;
+    en = 0;
+    a = 0;
+    b = 0;
+
+    // Reset
+    #20; rst_n = 1;
+
+    // Test 1: 3*4
+    @(posedge clk);
+    en = 1;
+    a = 3;
+    b = 4;
+
+    @(posedge clk);
+    en = 0;
+
+    #10;
+    $display("Acc = %0d , Expected = 12", acc);
+
+        // Test 2: 5*-2
+    @(posedge clk);
+    en = 1;
+    a = 5;
+    b = -2;
+
+    @(posedge clk);
+    en = 0;
+
+    #10;
+    $display("Acc = %0d , Expected = 2", acc);
+
+    #20;
+    $finish;
   end
 endmodule
