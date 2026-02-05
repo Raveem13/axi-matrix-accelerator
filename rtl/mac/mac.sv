29f42cbf528f9e28c3ed7b54dcc83ae9f7ff10f1
Raveem KG <gkraveem@gmail.com>
2026-02-05 16:27:08 +0530

    Removing wrong files

--- rtl/mac/mac.sv
+++ rtl/mac/mac.sv
@@ -1,23 +1,9 @@
-module mac #(
-    parameter A_WIDTH = 8,
-    parameter B_WIDTH = 8,
-    parameter ACC_WIDTH = 32
-)(
+module mac (
     input  logic clk,
-    input  logic rst_n,        //active-low reset
+    input  logic rst,
+    input  logic signed [15:0] a,
+    input  logic signed [15:0] b,
     input  logic en,
-
-    input  logic signed [A_WIDTH-1:0] a,
-    input  logic signed [B_WIDTH-1:0] b,
-    output logic signed [ACC_WIDTH-1:0] acc 
+    output logic signed [31:0] y
 );
-
-    always_ff @(posedge clk) begin
-        if (!rst_n) begin
-            acc <= 0;
-        end else if(en)begin
-            acc <= acc + (a * b);
-        end
-    end
-
 endmodule
--- tb/basic_tb.sv
+++ tb/basic_tb.sv
@@ -1,59 +1,12 @@
 module basic_tb;
-
-  logic clk;
-  logic rst_n;
-  logic en;
-  logic signed [7:0] a, b;
-  logic signed [31:0] acc;
-
-  // DUT
-  mac dut(
-    .clk(clk),
-    .rst_n(rst_n),
-    .en(en),
-    .a(a),
-    .b(b),
-    .acc(acc)
-  );
-
-  // Clock
+  logic clk = 0;
+  logic rst = 1;
   always #5 clk = ~clk;
 
+  top dut(.clk(clk), .rst(rst));
+
   initial begin
-    clk = 0;
-    rst_n = 0;
-    en = 0;
-    a = 0;
-    b = 0;
-
-    // Reset
-    #20; rst_n = 1;
-
-    // Test 1: 3*4
-    @(posedge clk);
-    en = 1;
-    a = 3;
-    b = 4;
-
-    @(posedge clk);
-    en = 0;
-
-    #10;
-    $display("Acc = %0d , Expected = 12", acc);
-
-        // Test 2: 5*-2
-    @(posedge clk);
-    en = 1;
-    a = 5;
-    b = -2;
-
-    @(posedge clk);
-    en = 0;
-
-    #10;
-    $display("Acc = %0d , Expected = 2", acc);
-
-    #20;
-    $finish;
+    #20 rst = 0;
+    #100 $finish;
   end
 endmodule
