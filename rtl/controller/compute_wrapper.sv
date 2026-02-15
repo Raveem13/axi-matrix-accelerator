042651a849a7c692e1afe4ac6f94a529e724d26d
Raveem KG <gkraveem@gmail.com>
2026-02-15 18:56:18 +0530

    D14: AXI-Stream output backpressure validation
    - Added Output buffer logic against stalls.
    - Verified data stability under stall
    - Confirmed valid/ready handshake correctness
    - Fixed over-constrained assertions & added some

--- rtl/controller/compute_wrapper.sv
+++ rtl/controller/compute_wrapper.sv
@@ -32,6 +32,7 @@ module compute_wrapper #(
     // Buffers
     logic [DATA_W-1:0] A_buf [2][K_MAX];
     logic [DATA_W-1:0] B_buf [K_MAX][2];
+    logic [DATA_W-1:0] C_buf [2][2];
 
     // Counter
     logic [$clog2(K_MAX):0] k_cnt;
@@ -39,6 +40,11 @@ module compute_wrapper #(
     logic [$clog2(K_MAX):0] b_cnt;
     logic [2:0] c_cnt;
 
+    
+    // Holding C data stable, register the output
+    logic [DATA_W-1:0] c_data_reg;
+    logic              c_valid_reg;
+    logic              c_last_reg;
 
     // fsm states
     typedef enum logic [2:0] {
@@ -67,8 +73,8 @@ module compute_wrapper #(
         s_axis_a_tready =   0;
         s_axis_b_tready =   0;
 
-        m_axis_c_tvalid =   0;
-        m_axis_c_tlast  =   0;
+        // c_valid_reg =   0;
+        // c_last_reg  =   0;
 
         done    =   0;
 
@@ -103,9 +109,9 @@ module compute_wrapper #(
             end
 
             OUTPUT  :   begin
-                m_axis_c_tvalid =   1;
+                // c_valid_reg =   1;
                 if (m_axis_c_tready &&  m_axis_c_tvalid && c_cnt == 3) begin
-                    m_axis_c_tlast  =   1;
+                    // c_last_reg  =   1;
                     next_state = DONE;
                 end
             end
@@ -145,7 +151,6 @@ module compute_wrapper #(
                 OUTPUT  :
                     if (m_axis_c_tvalid && m_axis_c_tready)
                         c_cnt <= c_cnt + 1;
-
                 default: begin
                     a_cnt <= 0;
                     b_cnt <= 0;
@@ -155,6 +160,33 @@ module compute_wrapper #(
             endcase
         end
     end
+
+    // Output buffering logic
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            c_data_reg  <= '0;
+            c_valid_reg <= 0;
+            c_last_reg  <= 0;
+        end else begin
+            if (state == OUTPUT && !c_valid_reg && m_axis_c_tready) begin
+                c_data_reg  <= c_cnt + 1;     //Place holder or MAC result
+                c_valid_reg <= 1;
+                c_last_reg  <= (c_cnt == 3);
+
+            end 
+            else if (c_valid_reg && m_axis_c_tready) begin
+                c_valid_reg <= 0; 
+                c_last_reg  <= 0;
+            end
+            // $display("%0t count_c = %0d, %S, c_valid =%0d, c_ready =%0d", $time, c_cnt, state.name(), c_valid_reg, m_axis_c_tready);
+            // $display("%0t %S, m_axis_: c_tvalid =%0d, c_tready =%0d, c_tdata = %0d, c_tlast=%0d", $time, state.name(), m_axis_c_tvalid, m_axis_c_tready, m_axis_c_tdata, m_axis_c_tlast);
+
+        end
+    end
+
+    assign m_axis_c_tdata   = c_data_reg;
+    assign m_axis_c_tvalid  = c_valid_reg;
+    assign m_axis_c_tlast   = c_last_reg;
 
     // assertions
     // No data accepted outside LOAD states
@@ -181,5 +213,58 @@ module compute_wrapper #(
     // assert property (@(posedge clk)
     //     done |=> state == DONE)
     //     else $fatal("assert fail: 'done' asserted not in DONE");
+
+    // Backpressure-aware assertions
+    // Data must remain stable while stalled
+    assert property (@(posedge clk)
+        $past(m_axis_c_tvalid) && m_axis_c_tvalid && !m_axis_c_tready |->
+        $stable(m_axis_c_tdata) && $stable(m_axis_c_tlast)
+    )
+        else $fatal("assert fail: data not stable while stalled");
+
+    // Counters only move on handshake
+    assert property (@(posedge clk)
+        (state == OUTPUT) && (c_cnt != $past(c_cnt)) |->
+        $past(m_axis_c_tvalid && m_axis_c_tready)
+    )
+    else $fatal("assert fail: c_counter =%0d on no-handshake", c_cnt);
+    // OR
+    // assert property (@(posedge clk)
+    //     (state == OUTPUT) &&
+    //     !$past(m_axis_c_tvalid && m_axis_c_tready)
+    //     |-> $stable(c_cnt)
+    // )
+    // else $fatal(1, "c_counter changed without handshake");
+    
+    // Exactly one tlast per burst  
+    assert property (@(posedge clk)
+        m_axis_c_tlast |-> (state == OUTPUT && c_cnt == 3)
+    )
+        else $fatal("assert fail: tlast not per burst");
+
+    // No output without start / No output outside OUTPUT state
+    assert property (@(posedge clk)
+        m_axis_c_tvalid |-> state == OUTPUT
+    )
+        else $fatal("assert fail: output outside OUTPUT state");
+
+    // VALID must not depend on READY
+    assert property (@(posedge clk)
+        disable iff (!rst_n)
+        $rose(m_axis_c_tvalid) |-> !$past(m_axis_c_tready) || 1'b1
+    );
+
+    // LAST only valid with VALID
+    assert property (@(posedge clk)
+        m_axis_c_tlast |-> m_axis_c_tvalid
+    )
+    else $fatal(1, "TLAST without TVALID");
+
+    // VALID deasserts only after handshake
+    assert property (@(posedge clk)
+        disable iff (!rst_n)
+        $fell(m_axis_c_tvalid) |-> $past(m_axis_c_tready)
+    )
+    else $fatal(1, "TVALID dropped without handshake");
 
 endmodule
