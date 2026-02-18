0d14572e9cd412cf2d046d8e1571ff8a383e1e42
Raveem KG <gkraveem@gmail.com>
2026-02-18 12:55:50 +0530

    D18: AXI-Lite Driver Protocol Implemented
    - Reset behavior
    - axi-lite write + read protocols added.

--- tb/axi_agent/axi_lite_driver.sv
+++ tb/axi_agent/axi_lite_driver.sv
@@ -1,4 +1,4 @@
-class axi_lite_driver extends uvm_driver #(axi_lite_driver);
+class axi_lite_driver extends uvm_driver #(axi_lite_item);
     `uvm_component_utils(axi_lite_driver)
 
     virtual axi_lite_if vif;
@@ -9,13 +9,95 @@ class axi_lite_driver extends uvm_driver #(axi_lite_driver);
 
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        if (!uvm_config_db#(virtual_axi_lite_if)::get(this, "","vif", vif))
+        if (!uvm_config_db#(virtual axi_lite_if)::get(this, "","vif", vif))
             `uvm_fatal("NOVIF", "AXI Interface not set")
     endfunction
 
     task run_phase(uvm_phase phase);
-        super.run_phase(phase);
         // Drive signals
-        
+        axi_lite_item tr;
+
+        reset_signals();
+
+        forever begin
+            seq_item_port.get_next_item(tr);
+
+            if (tr.is_write) begin
+                axi_write(tr);
+            end
+            else begin
+                axi_read(tr);
+            end
+
+            seq_item_port.item_done();
+        end
+
     endtask
+
+    task reset_signals();
+        vif.AWVALID <= 0;
+        vif.WVALID  <= 0;
+        vif.BREADY  <= 0;
+        vif.ARVALID <= 0;
+        vif.RREADY  <= 0;
+    endtask
+
+    // AXI-Lite write protocol
+    task axi_write(axi_lite_item tr);
+        // Address channel
+        vif.AWADDR  <= tr.addr;
+        vif.AWVALID <= 1;
+
+        // Data channel
+        vif.WADDR   <= tr.data;
+        vif.WSTRB  <= 4'hF;
+        vif.WVALID  <= 1;
+
+        // Handshake independently
+        fork
+            begin
+                @(posedge vif.ACLK);
+                wait (vif.AWREADY);
+                vif.AWVALID <= 0;
+            end
+            begin
+                @(posedge vif.ACLK);
+                wait (vif.WREADY);
+                vif.WVALID <= 0;
+            end
+        join
+
+        // Response channel
+            vif.BREADY <= 1;
+            @(posedge vif.ACLK);
+            wait (vif.BVALID);
+
+            tr.resp = vif.BRESP;
+
+            vif.BREADY <= 0;
+
+    endtask
+
+    // AXI-Lite read protocol
+    task axi_read(axi_lite_item tr);
+        // Address channel
+        vif.ARADDR  <= tr.addr;
+        vif.ARVALID <= 1;
+
+        // Handshake
+        @(posedge vif.ACLK);
+        wait (vif.ARREADY);
+        vif.ARVALID <= 0;
+
+        // Read Data channel
+        vif.RREADY <= 1;
+        @(posedge vif.ACLK);
+        wait (vif.RVALID);
+
+        tr.rdata= vif.RDATA;
+        tr.resp = vif.RRESP;
+
+        vif.RREADY <= 0;
+    endtask
+
 endclass //axi_lite_driver extends uvm_driver
